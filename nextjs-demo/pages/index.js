023d45ab72baffd510f31a0ea80a4cff69d72ccd
2Chaos <ace.wonder@qq.com>
2023-04-18 23:36:02 +0800

    test

--- nextjs-demo/pages/index.js
+++ nextjs-demo/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import App from "@2chaos/webpack_react_todolist";
+console.log("hi?");
 const TodolistPage = () => {
+  console.log("hello?");
   return (
     <div>
       <h1>Todo List</h1>
@@ -8,5 +10,5 @@ const TodolistPage = () => {
     </div>
   );
 };
-
+console.log("hello?");
 export default TodolistPage;
